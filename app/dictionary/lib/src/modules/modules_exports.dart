2579861a361c0262de07bd73fa269befeb0cdac9
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-08 20:50:11 -0300

    att modules

--- app/dictionary/lib/src/modules/modules_exports.dart
+++ app/dictionary/lib/src/modules/modules_exports.dart
@@ -5,3 +5,5 @@ export 'word/word_module.dart';
 export 'history/history_module.dart';
 export 'favorites/favorites_module.dart';
 export 'words/words_module.dart';
+export 'layout1/layout1_module.dart';
+export 'layout2/layout2_module.dart';

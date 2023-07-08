e7ebf9c8136053010488e9ab4655dba6741d7b5e
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-08 20:46:53 -0300

    att random color

--- app/dictionary/lib/src/app_theme.dart
+++ app/dictionary/lib/src/app_theme.dart
@@ -1,26 +1,28 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 ThemeData lightTheme() => ThemeData(
       useMaterial3: true,
       scaffoldBackgroundColor:
-          Color((math.Random().nextDouble() * 0xfdfdfd).toInt())
-              .withOpacity(0.9),
+          // Colors.primaries[Random().nextInt(Colors.primaries.length)],
+          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
       // floatingActionButtonTheme: const FloatingActionButtonThemeData(
       //   foregroundColor: Color.fromARGB(255, 255, 255, 255),
       // ),
-      colorScheme: const ColorScheme.light(
+      colorScheme: ColorScheme.light(
         brightness: Brightness.light,
-        primary: Color.fromARGB(255, 124, 40, 172),
-        secondary: Color.fromARGB(255, 202, 18, 18),
+        background: const Color.fromARGB(255, 255, 255, 255),
+        // primary: Color.fromARGB(255, 124, 40, 172),
+        primary:
+            Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
+        secondary: const Color.fromARGB(255, 202, 18, 18),
       ),
     );
 
 ThemeData darkTheme() => ThemeData(
       useMaterial3: true,
       scaffoldBackgroundColor:
-          Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
-              .withOpacity(0.1),
+          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.1),
       colorScheme: const ColorScheme.dark(
         brightness: Brightness.dark,
         primary: Color.fromARGB(255, 255, 255, 255),

cb14a7bbdd4cfd56f1ad803800a7706c30a48ef7
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-08 20:48:09 -0300

    att services

--- app/dictionary/lib/src/core/services/services_exports.dart
+++ app/dictionary/lib/src/core/services/services_exports.dart
@@ -2,3 +2,4 @@ export "firebase/firebase_exports.dart";
 export 'sentry/sentry_exports.dart';
 export 'shared_pref/shared_pref_exports.dart';
 export 'layout/layout_exports.dart';
+export 'json/json_exports.dart';

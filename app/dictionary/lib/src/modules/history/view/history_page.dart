ab064424d3827db10a85162a26856086391cb95d
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-08 21:45:44 -0300

    att history page

--- app/dictionary/lib/src/modules/history/view/history_page.dart
+++ app/dictionary/lib/src/modules/history/view/history_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
-  const HistoryPage ({super.key});
+  const HistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     // var height = MediaQuery.sizeOf(context).height;
     // var width = MediaQuery.sizeOf(context).width;
 
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HistoryPage"),
+      ),
+    );
   }
 }

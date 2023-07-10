4e81b3d96c6ea4ef40baf6376a3550a21eab8d80
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-10 04:25:40 -0300

    att fav page

--- app/dictionary/lib/src/modules/favorites/view/favorites_page.dart
+++ app/dictionary/lib/src/modules/favorites/view/favorites_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../../src_exports.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
@@ -8,10 +12,41 @@ class FavoritesPage extends StatelessWidget {
     // var height = MediaQuery.sizeOf(context).height;
     // var width = MediaQuery.sizeOf(context).width;
 
+    final favoritesStore = Modular.get<FavoritesStore>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("FavoritesPage"),
+        title: const Text("Favorites"),
       ),
+      body: Observer(builder: (_) {
+        return Scrollbar(
+          child: ListView.separated(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            itemCount: favoritesStore.favoritesList.length,
+            separatorBuilder: (_, index) => Divider(
+              color: Colors.white.withOpacity(0.5),
+            ),
+            itemBuilder: (_, index) {
+              String wordIndex = favoritesStore.favoritesList[index];
+
+              return Card(
+                child: ListTile(
+                    title: Text(
+                      wordIndex,
+                    ),
+                    trailing: IconButton(
+                      onPressed: () {
+                        favoritesStore.remove(wordIndex);
+                      },
+                      icon: const Icon(
+                        Icons.favorite,
+                      ),
+                    )),
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }

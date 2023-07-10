bcab233775f465b52df41127c798dfaa49bf0b20
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-10 07:08:47 -0300

    att firebase web config

--- app/dictionary/lib/src/core/services/firebase/firebase_opt_web.dart
+++ app/dictionary/lib/src/core/services/firebase/firebase_opt_web.dart
@@ -1,4 +1,4 @@
-const String apiKey = "\$FIREBASE_OPT_WEB_APIKEY";
-const String appId = "\$FIREBASE_OPT_WEB_APPID";
-const String messagingSenderId = "\$FIREBASE_OPT_WEB_MESSAGINGSENDERID";
-const String projectId = "\$FIREBASE_OPT_WEB_PROJECTID";
+const String apiKey = "AIzaSyBQPRF1LfKp7z8z1YhAkmL6SlZQp3cK8QY";
+const String appId = "1:1084350328261:web:763acd6c429236b413efac";
+const String messagingSenderId = "1084350328261";
+const String projectId = "dictionary-coodesh";

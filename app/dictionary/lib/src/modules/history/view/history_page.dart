a1db2a748fcd1e79ba09ba84f304fde0f418a98b
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-10 04:25:29 -0300

    att history page

--- app/dictionary/lib/src/modules/history/view/history_page.dart
+++ app/dictionary/lib/src/modules/history/view/history_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import '../../../src_exports.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
@@ -8,10 +12,35 @@ class HistoryPage extends StatelessWidget {
     // var height = MediaQuery.sizeOf(context).height;
     // var width = MediaQuery.sizeOf(context).width;
 
+    final historyStore = Modular.get<HistoryStore>();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HistoryPage"),
+        title: const Text("History"),
       ),
+      body: Observer(builder: (_) {
+        return Scrollbar(
+          child: ListView.separated(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            itemCount: historyStore.historyList.length,
+            separatorBuilder: (_, index) => Divider(
+              color: Colors.white.withOpacity(0.5),
+            ),
+            itemBuilder: (_, index) {
+              String wordIndex = historyStore.historyList[index];
+
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    wordIndex,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }

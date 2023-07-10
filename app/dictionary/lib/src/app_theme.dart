a7e4d7151e624a5ae90bc3c9e299d41f0b84ac07
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-10 06:15:47 -0300

    att app theme

--- app/dictionary/lib/src/app_theme.dart
+++ app/dictionary/lib/src/app_theme.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 
 ThemeData lightTheme() => ThemeData(
       useMaterial3: true,
-      scaffoldBackgroundColor:
-          // Colors.primaries[Random().nextInt(Colors.primaries.length)],
-          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
+      scaffoldBackgroundColor: Colors.white,
+      // Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      // Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
       // floatingActionButtonTheme: const FloatingActionButtonThemeData(
       //   foregroundColor: Color.fromARGB(255, 255, 255, 255),
+      // ),
+      primaryTextTheme: Typography.blackMountainView,
+      textTheme: Typography.blackMountainView,
+
+      // textTheme: const TextTheme(
+      //   bodyLarge: TextStyle(),
+      //   bodyMedium: TextStyle(),
+      //   bodySmall: TextStyle(),
+      // ).apply(
+      //   bodyColor: Colors.black,
+      //   displayColor: Colors.purple,
       // ),
       colorScheme: ColorScheme.light(
         brightness: Brightness.light,
@@ -15,29 +26,40 @@ ThemeData lightTheme() => ThemeData(
         // primary: Color.fromARGB(255, 124, 40, 172),
         primary:
             Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
-        secondary: const Color.fromARGB(255, 202, 18, 18),
+        onSurface: Colors.black,
+        // secondary: const Color.fromARGB(255, 202, 18, 18),
       ),
     );
 
 ThemeData darkTheme() => ThemeData(
       useMaterial3: true,
-      scaffoldBackgroundColor:
-          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(0.1),
-      colorScheme: const ColorScheme.dark(
+      scaffoldBackgroundColor: Colors.black,
+      primaryTextTheme: Typography.whiteMountainView,
+      textTheme: Typography.whiteMountainView,
+      // textTheme: const TextTheme(
+      //   bodyLarge: TextStyle(),
+      //   bodyMedium: TextStyle(),
+      //   bodySmall: TextStyle(),
+      // ).apply(
+      //   bodyColor: Colors.white,
+      //   displayColor: Colors.green,
+      // ),
+      colorScheme: ColorScheme.dark(
         brightness: Brightness.dark,
-        primary: Color.fromARGB(255, 255, 255, 255),
+        primary:
+            Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1),
         // primaryContainer: Color.fromARGB(255, 218, 171, 171),
         // onPrimaryContainer: Color.fromARGB(255, 0, 0, 0),
-        secondary: Color.fromARGB(255, 255, 255, 255),
+        // secondary: const Color.fromARGB(255, 255, 255, 255),
         // secondaryContainer: Color.fromARGB(255, 64, 156, 151),
         // onSecondaryContainer: Color.fromARGB(255, 0, 0, 0),
-        surface: Color.fromARGB(255, 199, 40, 40),
-        background: Color.fromARGB(255, 0, 0, 0),
-        error: Color.fromARGB(255, 199, 40, 40),
-        onPrimary: Color.fromARGB(255, 0, 0, 0),
-        onSecondary: Color.fromARGB(255, 0, 0, 0),
-        onSurface: Color.fromARGB(255, 255, 255, 255),
-        onBackground: Color.fromARGB(255, 255, 255, 255),
-        onError: Color.fromARGB(255, 255, 255, 255),
+        surface: const Color.fromARGB(255, 176, 80, 186),
+        background: const Color.fromARGB(255, 0, 0, 0),
+        // error: const Color.fromARGB(255, 199, 40, 40),
+        onPrimary: const Color.fromARGB(255, 255, 255, 255),
+        // onSecondary: const Color.fromARGB(255, 63, 106, 130),
+        onSurface: const Color.fromARGB(255, 255, 255, 255),
+        // onBackground: const Color.fromARGB(255, 38, 125, 156),
+        // onError: const Color.fromARGB(255, 100, 100, 100),
       ),
     );

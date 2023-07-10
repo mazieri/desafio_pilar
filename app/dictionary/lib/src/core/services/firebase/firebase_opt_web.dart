c17bff6dddec60e8ca2227e5a859b2f95e9efad7
mazieri <44025173+mazieri@users.noreply.github.com>
2023-07-10 06:33:33 -0300

    att firebase web config

--- app/dictionary/lib/src/core/services/firebase/firebase_opt_web.dart
+++ app/dictionary/lib/src/core/services/firebase/firebase_opt_web.dart
@@ -1,4 +1,4 @@
-const String apiKey = "FIREBASE_OPT_WEB_APIKEY";
-const String appId = "FIREBASE_OPT_WEB_APPID";
-const String messagingSenderId = "FIREBASE_OPT_WEB_MESSAGINGSENDERID";
-const String projectId = "FIREBASE_OPT_WEB_PROJECTID";
+const String apiKey = "\$FIREBASE_OPT_WEB_APIKEY";
+const String appId = "\$FIREBASE_OPT_WEB_APPID";
+const String messagingSenderId = "\$FIREBASE_OPT_WEB_MESSAGINGSENDERID";
+const String projectId = "\$FIREBASE_OPT_WEB_PROJECTID";

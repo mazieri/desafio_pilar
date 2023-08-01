79026b318dca4b103913bf046520516bc07b9d6b
Felipe Mazieri <44025173+mazieri@users.noreply.github.com>
2023-08-01 02:07:57 -0300

    Delete firebase_opt_web.dart

--- app/dictionary/lib/src/core/services/firebase/firebase_opt_web.dart
+++
@@ -1,4 +0,0 @@
-const String apiKey = "AIzaSyBQPRF1LfKp7z8z1YhAkmL6SlZQp3cK8QY";
-const String appId = "1:1084350328261:web:763acd6c429236b413efac";
-const String messagingSenderId = "1084350328261";
-const String projectId = "dictionary-coodesh";
